7d993ffdf21a1ddbacb1433c7a39cd2fc3eced65
Melric Lazaro <melriclazaro@gmail.com>
2025-01-20 11:36:49 +0000

    Updated Film Schema and Added Create functionality

--- api/models/film.py
+++ api/models/film.py
@@ -6,3 +6,5 @@ class Film(db.Model):
     title = db.Column(db.String(128), nullable = False)
     description = db.Column(db.Text, nullable = True)
     release_year = db.Column(db.Integer, nullable = False)
+    language_id = db.Column(db.Integer, nullable = False)
+    original_language_id = db.Column(db.Integer, nullable = True)
--- api/routes/films.py
+++ api/routes/films.py
@@ -3,6 +3,7 @@ from marshmallow import ValidationError
 
 from api.models import db
 from api.models.film import Film
+from api.schemas.actor import actor_schema
 from api.schemas.film import film_schema, films_schema
 
 # Create the blueprint and insert into app.py
@@ -13,3 +14,33 @@ films_router = Blueprint('films', __name__, url_prefix='/films')
 def read_all_films():
     films = Film.query.all() # Query the database
     return films_schema.dump(films)
+
+# GET Requests a specific film in the database
+@films_router.get('/<film_id>')
+def read_film(film_id):
+    film = Film.query.get(film_id)
+    if film:
+        return film_schema.dump(film), 200
+    else:
+        return jsonify({'message': 'Film Not Found'}), 404
+
+# Function to create a new film and insert into the database
+@films_router.post('/')
+def create_film():
+    # Retrieve the data using a json request
+    film_data = request.json
+    # Check to see if the JSON data is valid
+    try:
+        film_schema.load(film_data)
+    except ValidationError as err:
+        return jsonify(err.messages), 400
+
+    # Create a new Film model
+    film = Film(**film_data)
+    # Insert Record
+    db.session.add(film)
+    # Update the database
+    db.session.commit()
+
+    return film_schema.dump(film)
+
